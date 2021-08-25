331f437721ed175d2ba1c5443301c85435bbb263
matdan <matdangariepy@hotmail.com>
2021-08-25 18:20:42 -0400

    install.sh can now delete command tools with option -d

--- install.sh
+++ install.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env sh
 
+
+
 DIR="$(cd $(dirname $0) && pwd)"
+
+# DELETING COMMAND TOOLS
+if [ $1 = "-d" ]; then
+
+	echo "DELETING COMMAND TOOLS"
+
+	for ENTRY in $DIR/*
+	do
+		if [ $ENTRY = "${ENTRY}/install.sh" ]; then
+			continue
+		fi
+		rm "/usr/local/bin/$(basename $ENTRY)"
+		echo "REMOVING SYMLINK: $ENTRY"
+	done
+	exit
+fi
+
+# INSTALLING COMMAND TOOLS
+echo "INSTALLING COMMAND TOOLS"
 
 for ENTRY in $DIR/*
 do
@@ -9,6 +30,5 @@ do
 	fi
 	chmod a+x $ENTRY
 	ln -sf $ENTRY "/usr/local/bin/$(basename $ENTRY)"
-	echo $ENTRY
+	echo "ADDING SYMLINK: $ENTRY"
 done
-
