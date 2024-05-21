9928f6b2b898b2aada5f006a8edc1109d91d2ebd
matdan9 <matdangariepy@hotmail.com>
2024-05-20 20:43:55 -0400

    don't know what happened

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-
 DIR="$(cd $(dirname $0) && pwd)"
 
 # DELETING COMMAND TOOLS
@@ -11,40 +10,6 @@ if [ $1 = "-d" ]; then
 	for ENTRY in $DIR/*
 	do
 		if [ $ENTRY = "${ENTRY}/install.sh" ]; then
-#!/usr/bin/env sh
-
-
-
-DIR="$(cd $(dirname $0) && pwd)"
-
-# DELETING COMMAND TOOLS
-if [ $1 = "-d" ]; then
-
-	echo "DELETING COMMAND TOOLS"
-
-	for ENTRY in $DIR/*
-	do
-		if [ $ENTRY = "${ENTRY}/install.sh" ]; then
-			continue
-		fi
-		rm "/usr/local/bin/$(basename $ENTRY)"
-		echo "REMOVING SYMLINK: $ENTRY"
-	done
-	exit
-fi
-
-# INSTALLING COMMAND TOOLS
-echo "INSTALLING COMMAND TOOLS"
-
-for ENTRY in $DIR/*
-do
-	if [ $ENTRY = "${ENTRY}/install.sh" ]; then
-		continue
-	fi
-	chmod a+x $ENTRY
-	ln -sf $ENTRY "/usr/local/bin/$(basename $ENTRY)"
-	echo "ADDING SYMLINK: $ENTRY"
-done
 			continue
 		fi
 		rm "/usr/local/bin/$(basename $ENTRY)"
